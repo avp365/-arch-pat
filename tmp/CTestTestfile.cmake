5e254e14319fa1f8fc3d57df2a96a967888a8c3e
Andy <avp365@yandex.ru>
2023-09-13 12:35:55 +0000

    c-ci

--- tmp/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dev/cpp/arch-pat
-# Build directory: /home/dev/cpp/arch-pat/tmp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(helloworld "test_version")
-set_tests_properties(helloworld PROPERTIES  _BACKTRACE_TRIPLES "/home/dev/cpp/arch-pat/CMakeLists.txt;54;add_test;/home/dev/cpp/arch-pat/CMakeLists.txt;0;")
